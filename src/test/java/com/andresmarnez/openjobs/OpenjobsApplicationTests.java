dcca6c9660febc1d7755ffb0916b4d93f84a3093
Andrés Sánchez <andresmarnez@gmail.com>
2023-01-27 17:45:08 +0100

    Initial commit

---
+++ src/test/java/com/andresmarnez/openjobs/OpenjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresmarnez.openjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
